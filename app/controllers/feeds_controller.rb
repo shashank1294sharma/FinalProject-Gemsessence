ece4ae951227b536e8209987c2dd853a9fdcf0a0
shashank Demo Project <shashankssharma1294@gmail.com>
2017-09-12 18:18:47 +0530

    Feed created by user which is login and shown all the feeds on page

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,7 +1,36 @@
 class FeedsController < ApplicationController
+  before_action :set_feed , only: [:edit,:update,:show,:destroy]
 
 	def index
 		@feeds = Feed.all
 	end
+
+	def new
+		@feeds = Feed.new
+	end
+
+	def create
+		 @feeds = Feed.new(feed_params)
+		 if @feeds.save
+		 	flash[:success] = "Feed was succesfully created	"
+		 	redirect_to feed_path(@feeds)
+		 else
+		 	render 'new'
+		 end
+	end
+
+	def show
+	end
+  
+  private
+
+   def set_feed
+	 	@feeds = Feed.find(params[:id])
+   end
+
+	def feed_params
+	 	params.require(:feed).permit(:post)
+	 end
+
 end
 
